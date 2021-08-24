6372f6b53c559fac1eae177c9dd7051cc0a6a200
igomezv <neorelativista@gmail.com>
2021-08-24 17:08:24 -0500

    update test with plotters

--- testplots.py
+++ testplots.py
@@ -1,16 +1,9 @@
 from simplemc.DriverMC import DriverMC
 
-# "read all setting from .ini file"
-# inifile = "baseConfig.ini"
-#
-# analyzer = DriverMC(iniFile=inifile)
-# analyzer.executer()
-# #analyzer.postprocess()
-
-
-""" useful for short tests,
-    or when just a few settings customize it from here"""
-analyzer = DriverMC(analyzername="nested", model="LCDM", datasets="HD",
-                    chainsdir='/home/isidro/Documents/gitHub/simplemc_tests/simplemc/chains/')
-analyzer.executer(nlivepoints=10)
+outputpath = '/home/isidro/Documents/gitHub/simplemc_tests/simplemc/chains/'
+analyzer = DriverMC(analyzername="mcmc", model="LCDM", datasets="HD",
+                    getdist=True, corner=True, simpleplot=True, showfig=True,
+                    chainsdir=outputpath)
+# args are related with the analyzer
+analyzer.executer(nsamp=100, skip=0)
 analyzer.postprocess()
