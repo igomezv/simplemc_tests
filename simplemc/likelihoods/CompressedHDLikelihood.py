f4950cd79fc939d5f7f40dcb058df9b346929282
igomezv <neorelativista@gmail.com>
2021-10-30 03:47:15 -0500

    import main path to read data

--- simplemc/likelihoods/CompressedHDLikelihood.py
+++ simplemc/likelihoods/CompressedHDLikelihood.py
@@ -3,6 +3,7 @@
 from simplemc.likelihoods.BaseLikelihood import BaseLikelihood
 import scipy.linalg as la
 import scipy as sp
+from simplemc import cdir
 
 class CompressedHDLikelihood(BaseLikelihood):
     def __init__(self,name,values_filename, cov_filename):
@@ -56,5 +57,5 @@ class HubbleDiagram(CompressedHDLikelihood):
        #                                       "/home/cosmocicatais/Documents/github/neuralCosmoReconstruction/notebooks/autoEncoderHz_cov50_.dat")
 
 
-       CompressedHDLikelihood.__init__(self, "HD", "simplemc/data/HDiagramCompilacion-data_31.txt",
-                                        "simplemc/data/HDiagramCompilacion-cov_31.txt")
+       CompressedHDLikelihood.__init__(self, "HD", cdir+"/data/HDiagramCompilacion-data_31.txt",
+                                        cdir+"/data/HDiagramCompilacion-cov_31.txt")
