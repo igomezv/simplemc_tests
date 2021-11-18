d7ae2ba121982bf46524d5eeb87f1e7a8a985c14
igomezv <neorelativista@gmail.com>
2021-11-18 11:17:16 -0600

    move neuralike analyzers -> likelihoods

--- simplemc/analyzers/__init__.py
+++ simplemc/analyzers/__init__.py
@@ -3,5 +3,4 @@ from .MCMCAnalyzer import MCMCAnalyzer
 from .dynesty import DynamicNestedSampler, NestedSampler
 from .emcee import EnsembleSampler
 from .GA_deap import GA_deap
-from .neuralike import NeuralManager
 from .MCEvidence import MCEvidence
