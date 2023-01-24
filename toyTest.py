e847e47b1d8b3818da78557e2347dd741fe70580
igomezv <neorelativista@gmail.com>
2023-01-23 23:20:31 -0600

    working toy models with nested

--- toyTest.py
+++ toyTest.py
@@ -24,7 +24,7 @@ ti = time.time()
 res1 = sampler1.executer(nlivepoints=100)
 # resultnested = sampler1.results
 
-resultnested = res1['result']['samples']
+samplesnested = res1['result']['samples']
 tfnested = time.time() - ti
 
 # ###### SECOND SAMPLING WITH DYNESTY + NEURAL NETWORK
@@ -49,37 +49,36 @@ tfnested = time.time() - ti
 # print("\nTime dynesty: {:.4f} min | Time dynesty+ANN: {:.4f} min".format(tfnested/60, tfbambi/60 ))
 
 # ### Plot results if you aren't in a server
-# if show_plots:
-#     nestdata = np.loadtxt(modelname+'_dynesty_1.txt', usecols=(2, 3))
-#     # bambidata = np.loadtxt(modelname+'_bambi_1.txt', usecols=(2, 3))
-#     znest = np.zeros(len(nestdata))
-#
-#     # for i, row in enumerate(nestdata):
-#     #     znest[i] = tm.loglike(row)
-#
-#     # zbambi = np.zeros(len(bambidata))
-#     #
-#     # for i, row in enumerate(bambidata):
-#     #     zbambi[i] = tm.loglike(row)
-#     fig = plt.figure()
-#
-#     # 3D plots
-#     ax = fig.add_subplot(111, projection='3d')
-#
-#     ax.scatter(nestdata[:, 0], nestdata[:, 1],  znest, c='red', alpha=0.5)
-#     # ax.scatter(bambidata[:, 0], bambidata[:, 1], zbambi, c='green', alpha=0.5)
-#     # plt.legend(["dynesty", "dynesty + neural net"],  loc="upper right")
-#
-#     plt.savefig(modelname+"_bambi3D.png")
-#     plt.show()
-#
-#     # 2D plots
-#     fig = plt.figure()
-#     ax = fig.add_subplot(111)
-#     plt.scatter(nestdata[:, 0], nestdata[:, 1], c='red', alpha=0.5)
-#     # plt.scatter(bambidata[:, 0], bambidata[:, 1], c='green', alpha=0.5)
-#     plt.legend(["dynesty", "dynesty + neural net"],  loc="upper right")
-#     ax.set_aspect('equal', adjustable='box')
-#     # plt.savefig(modelname+"_bambi2D.png")
-#     plt.show()
+if show_plots:
+    # bambidata = np.loadtxt(modelname+'_bambi_1.txt', usecols=(2, 3))
+    znest = np.zeros(len(samplesnested))
+
+    # for i, row in enumerate(nestdata):
+    #     znest[i] = tm.loglike(row)
+
+    # zbambi = np.zeros(len(bambidata))
+    #
+    # for i, row in enumerate(bambidata):
+    #     zbambi[i] = tm.loglike(row)
+    fig = plt.figure()
+
+    # 3D plots
+    ax = fig.add_subplot(111, projection='3d')
+
+    ax.scatter(samplesnested[:, 0],samplesnested[:, 1],  znest, c='red', alpha=0.5)
+    # ax.scatter(bambidata[:, 0], bambidata[:, 1], zbambi, c='green', alpha=0.5)
+    # plt.legend(["dynesty", "dynesty + neural net"],  loc="upper right")
+
+    # plt.savefig(modelname+"_bambi3D.png")
+    plt.show()
+
+    # 2D plots
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    plt.scatter(samplesnested[:, 0], samplesnested[:, 1], c='red', alpha=0.5)
+    # plt.scatter(bambidata[:, 0], bambidata[:, 1], c='green', alpha=0.5)
+    plt.legend(["dynesty", "dynesty + neural net"],  loc="upper right")
+    ax.set_aspect('equal', adjustable='box')
+    # plt.savefig(modelname+"_bambi2D.png")
+    plt.show()
 
