4d118fb5737b105cd2fa74563869a5aabd28fb9e
igomezv <neorelativista@gmail.com>
2023-03-05 01:19:22 -0600

    return to adam

--- simplemc/analyzers/neuralike/NeuralNet.py
+++ simplemc/analyzers/neuralike/NeuralNet.py
@@ -111,8 +111,8 @@ class NeuralNet:
         # if self.bayesian:
         #     kl_loss = bnn.BKLLoss(reduction='mean', last_layer_only=False)
         #     kl_weight = 0.01
-        # optimizer = torch.optim.Adam(self.model.parameters(), lr=self.learning_rate)
-        optimizer = torch.optim.Adadelta(self.model.parameters(), lr=self.learning_rate, weight_decay=1e-5)
+        optimizer = torch.optim.Adam(self.model.parameters(), lr=self.learning_rate)
+        # optimizer = torch.optim.Adadelta(self.model.parameters(), lr=self.learning_rate, weight_decay=0.01)
         # optimizer = torch.optim.SGD(self.model.parameters(), lr=self.learning_rate, momentum=0.9, weight_decay=1e-5)
         # optimizer = AdaBound(self.model.parameters(), lr=self.learning_rate, final_lr=0.01, weight_decay=1e-10, gamma=0.1)
         # optimizer = torch.optim.Adagrad(self.model.parameters(), lr=self.learning_rate,
