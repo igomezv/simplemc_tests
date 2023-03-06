373226a27f1120ab12b22b0eb3b67ce22eeb0d5f
igomezv <neorelativista@gmail.com>
2023-03-06 05:27:39 -0600

    add ann ini file

--- neuralike_test.py
+++ neuralike_test.py
@@ -1,12 +1,20 @@
 from simplemc.DriverMC import DriverMC
 
 "read all setting from .ini file"
-inifile = "neuralikeConfig.ini"
+inifileANN = "neuralikeConfig_ANN.ini"
+inifileGA = "neuralikeConfig_GA.ini"
+inifileANNGA = "neuralikeConfig_ANN_GA.ini"
 
-analyzer = DriverMC(iniFile=inifile)
+
+analyzer = DriverMC(iniFile=inifileANN)
 analyzer.executer()
 #analyzer.postprocess()
 
+#analyzer = DriverMC(iniFile=inifileGA)
+#analyzer.executer()
+
+#analyzer = DriverMC(iniFile=inifileANNGA)
+#analyzer.executer()
 
 """ useful for short tests,
     or when just a few settings customize it from here"""
